3a34f46ddbcb2f12f1d03a2536b1fdb5bb60c2af
Magma <fireonmoney@gmail.com>
2026-03-04 00:59:26 +0800

    update sniffer and udp to wireshark

--- Sniffer_Anchor/Sniffer_Anchor.ino
+++ Sniffer_Anchor/Sniffer_Anchor.ino
@@ -4,6 +4,28 @@
 #define PIN_IRQ 34
 #define PIN_SS 4
 
+/*
+(未加密)
+Default communication configuration. We use default non-STS DW mode.
+*/
+/*
+static dwt_config_t config = {
+    5,                // Channel number.
+    DWT_PLEN_128,     // Preamble length. Used in TX only.
+    DWT_PAC8,         // Preamble acquisition chunk size. Used in RX only.
+    9,                // TX preamble code. Used in TX only.
+    9,                // RX preamble code. Used in RX only.
+    1,                // 0 to use standard 8 symbol SFD, 1 to use non-standard 8 symbol, 2 for non-standard 16 symbol SFD and 3 for 4z 8 symbol SDF type
+    DWT_BR_6M8,       // Data rate.
+    DWT_PHRMODE_STD,  // PHY header mode.
+    DWT_PHRRATE_STD,  // PHY header rate.
+    129,              // SFD timeout (preamble length + 1 + SFD length - PAC size). Used in RX only.
+    DWT_STS_MODE_OFF, // STS disabled
+    DWT_STS_LEN_64,   // STS length see allowed values in Enum dwt_sts_lengths_e
+    DWT_PDOA_M0       // PDOA mode off
+};
+*/
+
 // 加密
 static dwt_config_t config = {
     5,                // Channel
